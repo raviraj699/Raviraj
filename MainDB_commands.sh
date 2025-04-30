213f356f5e0c4bc543713a747ec8631dc2b55dc5
Raj Abhishek <138127028+Rajcommit@users.noreply.github.com>
2025-04-30 21:22:11 +0530

    Update MainDB_commands.sh

--- MainDB_commands.sh
+++ MainDB_commands.sh
@@ -1,2 +1,5 @@
 1> aws rds describe-db-instances <Db-instance-identifier> ## List's Db instances based on identifier, if there are no identifier's provided it will list all db. Name of DB not identifier
-2> To find the particular DB in the rds "aws rds describe-db-instances --db-instance-identifier mydbinstance --region <region>"
+2> To find the particular DB in the rds "aws rds describe-db-instances --db-instance-identifier <Identity-fier> --region <region>"
+Diffrence between Endpoint and idendinetifier : 
+Endpoint : mydbinstance.ch820uu2q5wo.ap-south-1.rds.amazonaws.com 
+Identity-fier: mydbinstance
