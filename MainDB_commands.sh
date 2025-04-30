1ecc9b2fdeb7b2acde409fb6ef7f3f7f8f13285e
Raj Abhishek <138127028+Rajcommit@users.noreply.github.com>
2025-04-30 21:16:43 +0530

    Create MainDB_commands.sh

---
+++ MainDB_commands.sh
@@ -0,0 +1,2 @@
+1> aws rds describe-db-instances <Db-instance-identifier> ## List's Db instances based on identifier, if there are no identifier's provided it will list all db. Name of DB not identifier
+2> To find the particular DB in the rds "aws rds describe-db-instances --db-instance-identifier mydbinstance --region <region>"
